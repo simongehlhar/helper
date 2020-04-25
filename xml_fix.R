e11773bdc1f92a4b0fc994146208f8085f580431
Simon Gehlhar <simgeh@Simons-iMac.fritz.box>
2020-04-25 15:31:16 +0200

    made as function

--- xml_fix.R
+++ xml_fix.R
@@ -1,24 +1,30 @@
 library(xml2)
 library(magrittr)
-folders <- list.dirs("/Volumes/data/polmine/welt/xml_out", recursive = FALSE)
-#folders <- folders[47:length(folders)]
-for(folder in folders){
-  print(folder)
-  lapply(list.files(folder, full.names = TRUE), function(file){
-    tryCatch(a1 <- xml2::read_xml(file),
-             error = function(e){
-               print(file)
-               file_to_open <- file(file)
-               fix_to_write <- readLines(file_to_open) %>%
+
+fix_xml <- function(projekt_dir){
+  error_log <- file.path(projekt_dir, "error.log")
+  if(file.exists(error_log) == TRUE) file.remove(error_log)
+  folders <- list.dirs(file.path(projekt_dir, "tei"), recursive = FALSE)
+  for(folder in folders){
+    print(folder)
+    lapply(list.files(folder, full.names = TRUE), function(file){
+      tryCatch(a1 <- xml2::read_xml(file),
+               error = function(e){
+                 print(file)
+                 'con <- file(description = file)
+               fix_to_write <- readLines(con) %>%
                  gsub("&", "und", .) %>%
                  gsub("<<", "<", .) %>%
                  gsub(">>", ">", .) %>%
                  gsub(">< ", ">", .) %>%
                  gsub(" ><", "<", .) %>%
                  gsub("<7i>|< b>|<7I>|<-2>|<\\(i>|<7b>|<7b>|>>>|<<<", "", .)
-               writeLines(fix_to_write, file_to_open)
-               close(file_to_open)
-               write(file, "/Users/simgeh/error.txt", append = TRUE)
-             })
-  })
+               close(con)'
+                 #writeLines(fix_to_write, file)
+                 write(file, error_log, append = TRUE)
+               })
+    })
+  }
 }
+
+fix_xml("/Users/simgeh/lab/_temp/DieZeit")
